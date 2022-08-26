5cfd417c88ba6f2d0ffd4a7e67d9eebd3e541bbb
lhidalgogt2003 <luigihg592@gmail.com>
2022-08-26 12:22:54 +0200

    Ionic Data Update

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'mock-server';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:3000/api/users').subscribe((data) => {
+      console.log(data);
+    })
+  }
+}
